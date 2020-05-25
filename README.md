f12462010d5504a1db75d80eaa9d0e7d8030b42d
Roger Williams <roger@wilcis.com>
2020-05-25 13:48:32 -0400

    Updated problem 4 and added README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+# Euler Solutions
+## Not Sharing Solutions Policy
+According to the Euler site, it is not allowed to share solutions
+outside of the first 100.  As I don't anticipate getting through that
+many those above 100 will never appear here.
+
+These are not be solved to simply obtain the solution and move on to
+the next one.  I'm using these for personal instructional purposes in
+order to learn Clisp syntax.  Feel free to learn from these as you
+wish but please be aware of this purpose is for my personal training.
+Attempt to figure them out for yourself before reviewing here.
+
+Regards,
+
+Roger
+
--- prob4Palindrome.lisp
+++ prob4Palindrome.lisp
@@ -13,14 +13,21 @@
            reversed_num
            nil)))
 
-(defun largest-possible-product (number-of-ints)
-  (defvar string-of-int)
-  (setf string-of-int "")
-  (loop
-     for number-of-ints
-     do
-       (setf string-of-int 
-             (add-to-string string-of-int "9"))))
-
-(defun add-to-string (string addition)
-  (concatenate 'string string addition))
+(defun largest-possible-product ()
+  (let ((prod 0) (num1 999) (num2 999) (cur-largest-palindrome 0))
+    (loop while (> num1 0) do 
+      (loop while (> num2 0) do
+        (progn
+          ;;(print "Num1")
+          ;;(write num1)
+          ;;(print "Num2")
+          ;;(write num2)
+          (setq prod (* num1 num2))
+          (if (is-palindrome? prod)
+              (if (> prod cur-largest-palindrome)
+                  (setq cur-largest-palindrome prod)))
+          (setq num2 (- num2 1))))
+      (setq num2 999)
+      (setq num1 (- num1 1)))
+    (print "Largest Product Palindrome = ")
+    (write cur-largest-palindrome)))
