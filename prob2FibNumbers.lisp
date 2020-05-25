ba30ce14a9f93afca991b1a9e36115c27270a99e
Roger W. Williams <roger@wilcis.com>
2020-05-25 11:27:54 -0400

    Refactoring simplifications

--- prob2FibNumbers.lisp
+++ prob2FibNumbers.lisp
@@ -1,34 +1,25 @@
 (defparameter *MAX-RESULT* 4000000)
 
-(defun add (operand1 operand2) 
-  (+ operand1 operand2))
-
 (defun is-even (number)
-  (cond ((equal 0 number) 1)
-        ((equal 0 (mod number 2)) 1)
-        ((equal 1 (mod number 2)) 0)
-        (t ()))
-)
+  ;; Checks number for eveness
+  ;;   - is even  - 1
+  ;;   - not even - 0
+  (if (equal 0 (rem number 2)) 1 0)) 
 
 (defun fibinocci (number)
-  (cond (( equal 0 number )  0)
-        (( equal 1 number )  1)
-        (t   ( + (fibinocci (- number 2)) (fibinocci (- number 1))))
-        ))
+  (cond
+    (( equal 0 number )  0)
+    (( equal 1 number )  1)
+    (t   ( + (fibinocci (- number 2)) (fibinocci (- number 1))))))
 
-(defun sum-first-4M-fibs (max-sum)
-  (defvar counter 0)
-  (defvar total-sum 0)
-  (defvar fib-value 0)
-  (setf counter 0)
-  (setf total-sum 0)
-  (setf fib-value 0)
-  (loop
-     (when (< max-sum total-sum)
-       (print total-sum)
-       (return))
-     (setf fib-value (fibinocci counter))
-     (setf counter (add counter 1))
-     (if (equal 1 (is-even fib-value))
-         (setf total-sum (add total-sum fib-value)))
-     ))
+(defun sum-first-4M-fibs-even-valued (max-sum)
+  ;; Enter the number of even Fib numbers to sum
+  (let ((counter 0) (total-sum 0) (fib-value 0))
+    (loop
+      (when (< max-sum total-sum)
+        (print total-sum)
+        (return))
+      (setf fib-value (fibinocci counter))
+      (setf counter (+ counter 1))
+      (if (equal 1 (is-even fib-value))
+          (setf total-sum (+ total-sum fib-value))))))
